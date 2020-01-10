e02d54e5ddad6a5585dcc6e6c2dafc57f848bb52
kristie <kristiemconnor@gmail.com>
2020-01-09 20:01:03 -0500

    shrinks circle because of overlap, I couldn't correctly manipulate z

--- triangle.rb
+++ triangle.rb
@@ -6,6 +6,7 @@ Triangle.new(
   x1: 320, y1: 50,
   x2: 540, y2: 430, 
   x3: 100, y3: 430,
+  z: 1, 
   color: ['purple', 'green', 'yellow']
 )
 
@@ -17,8 +18,8 @@ t.color = 'yellow'
 
 Circle.new(
   x: 200, y:175,
-  radius: 150,
+  radius: 10,
   sectors: 32,
   color: 'fuchsia',
-  z: 3000)
+  z: 91000)
 show
